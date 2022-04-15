ebf724da9fc69fc8fd8894e8b9bcca2496083d46
Robot <2934026040@qq.com>
2022-04-15 20:54:04 +0800

    增加

--- test.c
+++ test.c
@@ -3,7 +3,16 @@ int main()
 {
 	int a = 10;
 
-	int ch[] = "Hello world!";
+	char ch[] = "Hello world!";
+	int *p = a;
+	for(int i = ；i < 10;i++)
+	{
+		a += i;
+		printf("%d  ____\n",*p);
+		p++;
+
+	}
+
 	printf("%d  ___   %c\n",a,ch);
 	return 0;
 }
