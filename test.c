086092a9b71b99f710c583377dd44006dcbdb2f2
Robot <2934026040@qq.com>
2022-04-15 22:04:37 +0800

    C5

--- test.c
+++ test.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
+int dev(char*p);
 int main()
 {
 	int a = 10;
 
 	char ch[] = "Hello world!";
+	printf("__%s\n");
 	int *p = a;
 	for(int i = ；i < 10;i++)
 	{
@@ -16,7 +18,18 @@ int main()
 	{
 		getchar();
 	}
+	edv(ch);
+	printf("__%s\n",ch);
 
 	printf("%d  ___   %c\n",a,ch);
 	return 0;
+}
+int dev(char*p)
+{
+	while(*p)
+	{
+		*p += ' ';
+		p++;
+	}
+
 }
